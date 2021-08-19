1bcbc5b1d8f6b22bf2b9918bd5bc69cdc67e8d56
Gitthuma <george.githuma@gmail.com>
2021-08-19 11:47:39 +0300

    print arithimetic

--- working-with-numbers.py
+++ working-with-numbers.py
@@ -2,7 +2,13 @@
 # print(7)
 
 # print a decimal number or float
-print(7.54321)
+# print(7.54321)
 
 # print a negative number
-print(-7.54321)
+# print(-7.54321)
+
+# print arithimetic
+print(5 + 2)
+print(5 - 2)
+print(5 * 2)
+print(5 / 2)
